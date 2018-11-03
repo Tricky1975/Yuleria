f7ee83337ce7a5bdefe2ba13728bd29546cdf9b2
Tricky1975 <facebook@tbbs.nl>
2018-11-03 23:00:02 +0100

    Setup and destroy set up better

--- yuleria.c
+++ yuleria.c
@@ -3,29 +3,30 @@
 #include "yuleria.h"
 
 void * yul_xnew(void * obj,long int size,void (* constructor)(void * self), void (* destructor)(void * self)){
-	void  * rob      = malloc(size); // rob = returnable object
+	void  * rob      = obj; //malloc(size); // rob = returnable object
 	yul_obj ret      = malloc(sizeof(struct yul_s_object));
 	ret->yobject     = rob;
 	ret->ycount      = 1;
 	ret->ydestructor = destructor;
+	ret->ysize       = size;
 	if (constructor!=NULL) constructor(rob);
 	return ret; // warning preventer (will be removed later);
 }
 
-void yul_null(yul_obj* obj){
-	yul_obj o=*obj;
+void _yulnull_do(yul_obj obj){
+	yul_obj o=obj;
 	o->ycount--;
 	if (o->ycount==0){
 		if (o->ydestructor!=NULL) o->ydestructor(o->yobject);
 		free(o->yobject);
 		free(o);
 	}
-	obj=NULL;
+	//obj=NULL;
 }
 
 void yul_point(yul_obj yobjtar,yul_obj yobjfrom){
 	if (yobjtar!=yobjfrom){
-		yul_null(&yobjtar);
+		yul_null(yobjtar);
 		yobjtar=yobjfrom;
 		yobjtar->ycount++;
 	}
--- yuleria.h
+++ yuleria.h
@@ -1,13 +1,15 @@
 #ifndef __YULERIA_HAS_ALREADY_BEEN_LOADED__
 #define __YULERIA_HAS_ALREADY_BEEN_LOADED__
-#define yul_new(size) yul_xnew(size,NULL,NULL)
+#define yul_new(size) yul_xnew(malloc(size),size,NULL,NULL)
 #define yul_object    yul_obj
+#define yul_null(obj) _yulnull_do(obj); obj=NULL;
 
 // types
 typedef struct yul_s_object {
 	void * yobject;
 	void (* ydestructor)(void * yself);
 	int    ycount;
+	int    ysize;
 } * yul_obj;
 
 typedef struct yul_s_node {
@@ -18,7 +20,10 @@ typedef struct yul_s_node {
 
 // functions
 void * yul_xnew(void * obj,long int size,void (* constructor)(void * self), void (* destructor)(void * self));
-void yul_null(yul_obj* obj);
+void _yulnull_do(yul_obj obj); // should never be used directly but through the macro!
 void yul_point(yul_obj yobjtar,yul_obj yobjfrom);
 
+
 #endif
+
+// TODO:total clean
